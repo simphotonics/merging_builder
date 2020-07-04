c9b87d01d5f9da71babe8eb084995d2ddda82ce4
simphotonics <git@simphotonics.com>
2020-07-04 20:01:17 +0100

    Added exports synthetic_input, standalone_builder.

--- lib/merging_builder.dart
+++ lib/merging_builder.dart
@@ -3,3 +3,5 @@ library merging_builder;
 
 export 'src/builders/merging_builder.dart';
 export 'src/generators/merging_generator.dart';
+export 'src/builders/standalone_builder.dart';
+export 'src/builders/synthetic_input.dart';
