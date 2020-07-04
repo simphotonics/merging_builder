8853756c460918684673ac23bcc73997a9f2085a
simphotonics <git@simphotonics.com>
2020-07-04 20:10:02 +0100

    Added import directive for Formatter.

--- test/src/mock_builders/mock_merging_builder.dart
+++ test/src/mock_builders/mock_merging_builder.dart
@@ -1,5 +1,6 @@
 import 'package:dart_style/dart_style.dart';
 import 'package:merging_builder/merging_builder.dart';
+import 'package:merging_builder/src/builders/formatter.dart';
 import 'package:source_gen/source_gen.dart';
 
 /// Mock builder with methods: [mergedContent] and [_combinedStream].
