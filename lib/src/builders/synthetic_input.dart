7a916bdf06ca2eed2a6380fa8fcd82e9180d4ea9
simphotonics <git@simphotonics.com>
2020-07-11 12:00:38 +0100

    Amended method instance<T>().

--- lib/src/builders/synthetic_input.dart
+++ lib/src/builders/synthetic_input.dart
@@ -1,5 +1,7 @@
 import 'package:meta/meta.dart';
 
+import '../errors/builder_error.dart';
+
 /// Base class representing synthetic builder input.
 /// For more information about synthetic input see:
 /// [Writing an Aggregate Builder](https://github.com/dart-lang/build/blob/master/docs/writing_an_aggregate_builder.md#writing-the-builder-using-a-synthetic-input).
@@ -15,13 +17,37 @@ abstract class SyntheticInput {
   @override
   String toString() => value;
 
-  /// Returns an instance of [Lib] or [Package].
+  /// Returns an instance of [$Lib$] or [$Package$].
   ///
   /// Note: Returns [null] is T does not extend [Synthetic].
   static SyntheticInput instance<T extends SyntheticInput>() {
-    if (T == Lib) return Lib();
-    if (T == Package) return Package();
-    return null;
+    return (T == $Lib$) ? $Lib$() : $Package$();
+  }
+
+  /// Returns [true] if [path] is a valid input or output path.
+  ///
+  /// Note: Synthetic input [$Lib$] supports only input/output files located
+  /// below the package directory `lib`.
+  static bool isValidPath<T extends SyntheticInput>(String path) {
+    if (T == $Lib$ && path.substring(0, 'lib'.length) != 'lib') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  /// Validates an input/output path.
+  ///
+  /// Throws [BuilderError] if the synthetic input is `r\lib/$lib$'\'`
+  /// (type parameter is [$Lib$]) and the path does not start with `lib`.
+  static void validatePath<T extends SyntheticInput>(String path) {
+    if (!SyntheticInput.isValidPath<T>(path)) {
+      throw BuilderError(
+          message: 'Invalid file path found.',
+          expectedState: 'A path starting with \'lib\'.'
+              'To access files outside \'lib\' change the builder type parameter to [Package].',
+          invalidState: 'The actual path is: $path.');
+    }
   }
 }
 
@@ -29,13 +55,13 @@ abstract class SyntheticInput {
 /// For more information about synthetic input see:
 /// [Writing an Aggregate Builder](https://github.com/dart-lang/build/blob/master/docs/writing_an_aggregate_builder.md#writing-the-builder-using-a-synthetic-input).
 @sealed
-class Lib extends SyntheticInput {
-  const Lib._(String value) : super._(value);
+class $Lib$ extends SyntheticInput {
+  const $Lib$._(String value) : super._(value);
 
-  static Lib _instance;
+  static $Lib$ _instance;
 
-  factory Lib() {
-    return _instance ?? Lib._(r'lib/$lib$');
+  factory $Lib$() {
+    return _instance ??= $Lib$._(r'lib/$lib$');
   }
 
   @override
@@ -46,13 +72,13 @@ class Lib extends SyntheticInput {
 /// For more information about synthetic input see:
 /// [Writing an Aggregate Builder](https://github.com/dart-lang/build/blob/master/docs/writing_an_aggregate_builder.md#writing-the-builder-using-a-synthetic-input).
 @sealed
-class Package extends SyntheticInput {
-  const Package._(String value) : super._(value);
+class $Package$ extends SyntheticInput {
+  const $Package$._(String value) : super._(value);
 
-  static Package _instance;
+  static $Package$ _instance;
 
-  factory Package() {
-    return _instance ?? Package._(r'$package$');
+  factory $Package$() {
+    return _instance ??= $Package$._(r'$package$');
   }
 
   @override
