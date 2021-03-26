68e4a65466ed110c2f29d5e016d4bd3586bca593
simphotonics <git@simphotonics.com>
2021-03-26 11:01:43 +0000

    Amended docs.

--- lib/src/builders/formatter.dart
+++ lib/src/builders/formatter.dart
@@ -1,7 +1,6 @@
-/// Typedef of a function that can be provided to the
+/// Typedef of a function that can be provided to
 /// [MergingBuilder] and [StandAloneBuilder]
-/// constructors in the form of the parameter [formatOutput].
-///
-/// The function is used to format the builder output before it
+/// constructors in the form of the parameter `formatter`.
+/// * The function is used to format the builder output before it
 /// is written to a file.
 typedef Formatter = String Function(String input);
