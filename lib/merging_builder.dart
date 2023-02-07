b47c69ecafc76f7f758056a24e06b1c0d8e53377
simphotonics <git@simphotonics.com>
2023-02-07 18:36:03 +0000

    Removed library name.

--- lib/merging_builder.dart
+++ lib/merging_builder.dart
@@ -1,6 +1,5 @@
 /// Library providing the builders [MergingBuilder] and [StandaloneBuilder].
 /// * Note: The builders use synthetic input.
-library merging_builder;
 
 export 'src/builders/merging_builder.dart';
 export 'src/generators/merging_generator.dart';
